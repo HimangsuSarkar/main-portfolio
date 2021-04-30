387c90d519b1da6f17769c971ffc378ff788921c
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-01 07:45:57 +0800

    design home and about page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer id="main-footer">
-    Copyright &copy; 2020
-  </footer>
+      Copyright &copy;2021
+    </footer>
   );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Header.css';
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
@@ -12,45 +12,45 @@ const Header = () => {
 
   return (
     <header>
-    <div className={`menu-btn ${showMenu ? 'close' : ''}`} onClick={toggleMenu}>
-      <div className="btn-line"></div>
-      <div className="btn-line"></div>
-      <div className="btn-line"></div>
-    </div>
-
-    <nav className={`menu ${showMenu ? 'show' : ''}`}>
-      <div className={`menu-branding ${showMenu ? 'show' : ''}`}>
-        <div className="portrait"></div>
+      <div className={`menu-btn ${showMenu ? 'close' : ''}`} onClick={toggleMenu}>
+        <div className="btn-line"></div>
+        <div className="btn-line"></div>
+        <div className="btn-line"></div>
       </div>
-      <ul className={`menu-nav ${showMenu ? 'show' : ''}`}>
-        <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
-          <Link to="home" className="nav-link">
-            Home
+
+      <nav className={`menu ${showMenu ? 'show' : ''}`}>
+        <div className={`menu-branding ${showMenu ? 'show' : ''}`}>
+          <div className="portrait"></div>
+        </div>
+        <ul className={`menu-nav ${showMenu ? 'show' : ''}`}>
+          <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
+            <Link to="home" className="nav-link">
+              Home
           </Link>
-        </li>
-        <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
-          <Link to="about" className="nav-link">
-            About Me
+          </li>
+          <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
+            <Link to="about" className="nav-link">
+              About Me
           </Link>
-        </li>
-        <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
-          <Link to="work" className="nav-link">
-            My Work
+          </li>
+          <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
+            <Link to="work" className="nav-link">
+              My Work
           </Link>
-        </li>
-        <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
-          <Link to="blog" className="nav-link">
-            Blog
+          </li>
+          <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
+            <Link to="blog" className="nav-link">
+              Blog
           </Link>
-        </li>
-        <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
-          <Link to="contact" className="nav-link">
-            Contact Me
+          </li>
+          <li className={`nav-item ${showMenu ? 'show' : ''}`} onClick={toggleMenu}>
+            <Link to="contact" className="nav-link">
+              Contact Me
           </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
