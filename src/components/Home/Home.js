d5c1afe37c95d2cf4f0326e6d4a378a16955b386
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-01 15:50:17 +0800

    footer create

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faFacebookSquare, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookSquare, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import Typical from 'react-typical';
 
 const Home = () => {
